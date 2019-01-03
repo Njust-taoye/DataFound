8f1b894ed2013727a3f4b437e3d51394fa75c844
Njust-taoye <2210703063@qq.com>
2019-01-03 23:37:53 +0800

    增加运行代码脚本

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## File Name: run.sh
+## Author: taoye01
+## Mail: taoye01@baidu.com
+## Copyright (c) 2012-2019 Baidu.com, Inc. All Rights Reserved
+## Created Time: Thu 03 Jan 2019 11:14:29 PM CST
+
+python -u feature.py --addWeekday True \
+				  --NGram_num 1000
+
+
+
+
