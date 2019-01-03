570a758246a8b775223cf0ddc34315db12d27aa5
Njust-taoye <2210703063@qq.com>
2019-01-03 23:44:59 +0800

    增加运行代码脚本

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ## File Name: run.sh
 ## Author: taoye01
-## Mail: taoye01@baidu.com
-## Copyright (c) 2012-2019 Baidu.com, Inc. All Rights Reserved
 ## Created Time: Thu 03 Jan 2019 11:14:29 PM CST
 
 python -u feature.py --addWeekday True \
