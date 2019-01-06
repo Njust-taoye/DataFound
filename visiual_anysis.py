204bc28edd42272a24496a998a7aa0d6a8dfef18
Njust-taoye <2210703063@qq.com>
2019-01-07 01:19:47 +0800

    优化代码，增加画图通用性

--- visiual_anysis.py
+++ visiual_anysis.py
@@ -16,13 +16,25 @@ import argparse
 
 
 parse = argparse.ArgumentParser()
-parse.add_argument("-f","--filename", help="need to plot filename", type=str)
+parse.add_argument("-f","--Filename", help="need to plot filename", type=str)
 parse.add_argument("-x", "--X_var", help="X coordinate variable name", type=str)
 parse.add_argument("-y", "--Y_var", help="Y coordinate variable name", type=str)
+parse.add_argument("-k", "--Kind", help="Chart type", type=str, default='line')
+parse.add_argument("-xs", "--X_scale", help="X-coordinate scale", type=int, default=10)
+parse.add_argument("-w", "--Width", help="figure With", type=int, default=0)
+parse.add_argument("-hi", "--High", help="figure High", type=int, default=0)
+
 args = parse.parse_args()
-filename = args.filename
+filename = args.Filename
 X_var = args.X_var
 Y_var = args.Y_var
+Kind = args.Kind
+X_scale = args.X_scale
+W = args.Width
+H = args.High
+figsize = (W, H)
+if W == 0 or H == 0:
+    figsize = None
 df = pd.read_csv(filename, sep='\t', header=0)
 df = df[(df['time'] > "2015-06-25 00:00:00") & (df['time'] < "2015-07-01 00:00:00")]
 print df.head()
@@ -31,14 +43,8 @@ df['words_num'] = df['content'].apply(lambda x: len(x.replace("\n", "").replace(
 df['set_words_num'] = df['content'].apply(lambda x:len(set(x.replace("\n", "").replace(" ","").decode('utf-8'))))
 sub_df = df[['content', 'fc', 'cc' ,'lc', 'words_num', 'set_words_num']]
 sub_df.sort_values(by=[X_var], inplace=True)
-#print sub_df[sub_df['fc']>6000]
-#plt.figure(figsize=(20,15),facecolor='w')##设置大小和背景颜色
-## 我们下面绘制的四幅图都是用的上面同一个plt，故下面四条线都在一张图中显示，如果想在不同图中显示，只需要在plt.plot之前重新定义一个figsize即可。
-#plt.plot(sub_df['words_num'].values, sub_df['fc'].values, 'r-', label='fc', linewidth=2)
-#plt.plot(sub_df['words_num'], sub_df['cc'], 'g-', label='cc', linewidth=2)
-#plt.plot(sub_df['words_num'], sub_df['lc'], 'b-', label='lc', linewidth=2)
 plt.grid()
-sub_df.plot(x=X_var, y=Y_var)
+sub_df.plot(figsize=figsize, x=X_var, y=Y_var, xlim=(min(df[X_var]), max(df[X_var])), xticks=(range(min(df[Y_var]), max(df[Y_var]), X_scale)), kind=Kind)
 plt.legend(loc='upper right') ## 图例的位置
-plt.savefig(X_var+"_"+Y_var+'.png')
-print "plot successed!!!!"
+plt.savefig(X_var+"_"+Y_var+"_"+Kind+'.png')
+print X_var+"_"+Y_var+"_"+Kind+'.png'+" "+ "ploted successed!!!!"
